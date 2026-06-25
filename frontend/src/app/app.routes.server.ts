f55cd257bdfa1417a188fe35d7b375f6b85e5dec
ssnkcode <ssnkcode@gmail.com>
2026-06-25 18:44:12 -0300

    Update

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -3,6 +3,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
+<<<<<<< Updated upstream
     renderMode: RenderMode.Prerender,
   },
+=======
+    renderMode: RenderMode.Server
+  }
+>>>>>>> Stashed changes
 ];
